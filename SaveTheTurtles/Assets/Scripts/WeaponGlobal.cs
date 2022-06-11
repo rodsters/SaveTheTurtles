5a830411a5e638c44e6741d1b128845021575924
Zacc <opslpo@gmail.com>
2022-06-11 15:15:45 -0700

    Visual Update to buttons
    buttons look nice and work somewhat

--- SaveTheTurtles/Assets/Scripts/WeaponGlobal.cs
+++ SaveTheTurtles/Assets/Scripts/WeaponGlobal.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponGlobal : MonoBehaviour
 {
     public int directionNum;
+    private void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
     public void Click()
     {
         Debug.Log("you fired in direction " + directionNum + " for " + StatHandler.range + " units far and " + StatHandler.spread + " units wide!" + "You probably hit a pedestrian!");
